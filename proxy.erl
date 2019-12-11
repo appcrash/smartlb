bb4a464d9e1baec54bd4a362403eb1449ad516a8
YH <cosmosgleam@gmail.com>
2019-12-11 22:38:22 +0800

    accept loop

--- proxy.erl
+++ proxy.erl
@@ -1,12 +1,9 @@
-#!/usr/bin/env escript
-
-
 -module(proxy).
 -behaviour(gen_server).
 
 -export([main/1,init/1,handle_cast/2,handle_call/3,code_change/3]).
 
--define(TCP_OPTIONS, [binary, {packet, 0}, {active, true}, {reuseaddr, true}]).
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
 
 -record(server_state,{
@@ -28,17 +25,44 @@ init(State) ->
   case gen_tcp:listen(Port,?TCP_OPTIONS) of
       {ok, Listen_Socket} ->
           NewState = State#server_state{listen_socket = Listen_Socket},
-          io:format("listen ok~n"),
-          {ok, accept_loop(NewState)};
+          Pid = spawn(fun() -> keep_process_loop(4,Listen_Socket) end),
+          register(kpl,Pid),
+          {ok, NewState};
 
       {error, Reason} -> {stop, Reason}
   end.
 
-accept_loop(_State) ->
-  % #server_state{port = Port} = State,
-  io:format("port is ~n").
+
+keep_process_loop(Remaining,Listen_Socket) when Remaining > 0 ->
+  io:format("keep process loop remaining ~p~n",[Remaining]),
+  spawn(fun() -> accept(Listen_Socket) end),
+  keep_process_loop(Remaining - 1,Listen_Socket);
+keep_process_loop(0,Listen_Socket) ->
+  receive
+    {accept_finished} -> keep_process_loop(1,Listen_Socket)
+  end.
+  
+
+accept(Listen_Socket) ->
+  case gen_tcp:accept(Listen_Socket) of
+    {ok,Socket} -> process_socket(Socket);
+    {error,Reason} -> io:format("accept error ~p~n",[Reason])
+  end,
+  kpl ! {accept_finished}.
+
+  
+
+process_socket(Socket) ->
+  case gen_tcp:recv(Socket,16) of
+    {ok,Packet} ->
+      io:format("packet is ~p",[Packet]),
+      process_socket(Socket);
+    {error,Reason} -> io:format("accept error ~p~n",[Reason])
+  end.
 
 
+  
+  
 handle_cast(stop,State) ->
   {stop,normal,State}.
 
