6e1e940e229fef589a156b586f50d47f875c6568
YH <cosmosgleam@gmail.com>
2019-12-13 15:57:26 +0800

    PoF finished, core function done

--- proxy.erl
+++ proxy.erl
@@ -3,13 +3,16 @@
 
 -export([main/1,init/1,handle_cast/2,handle_call/3,code_change/3]).
 
--define(TCP_OPTIONS, [list, {packet, 0}, {active, false}, {reuseaddr, true}]).
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 -define(SRC_PORT,7777).
 -define(DEST_PORT,9999).
+-define(INIT_PACKET_THRESHOLD,512).
+
 
 -record(server_state,{
   port,
   listen_socket = null
+
 }).
 
 
@@ -57,10 +60,15 @@ accept(Listen_Socket) ->
 socket_loop(Socket) ->
   receive
     {ready,Pid} ->
-      inet:setopts(Socket,[{active,true}]),
-      socket_loop(Socket,Pid)
+      socket_loop(Socket,Pid);
+    {ready_buffered,Pid,Buffered_Packet} ->
+      socket_loop(Socket,Pid,Buffered_Packet)
   end.
+socket_loop(Socket,Pid,Buffered_Packet) ->
+  gen_tcp:send(Socket,Buffered_Packet),
+  socket_loop(Socket,Pid).
 socket_loop(Socket,Pid) ->
+  inet:setopts(Socket,[{active,true}]),
   receive
     {tcp,Socket,Packet} ->
       Pid ! {send,Packet},
@@ -77,23 +85,53 @@ socket_loop(Socket,Pid) ->
   end.
 
 process_socket(Socket) ->
-  case gen_tcp:connect("localhost",?DEST_PORT,?TCP_OPTIONS) of
-    {ok,To_Socket} ->
-      P1 = spawn(fun() -> socket_loop(Socket) end),
-      P2 = spawn(fun() -> socket_loop(To_Socket) end),
+  case analyze_trait(Socket,<<>>) of
+    error ->
+      io:format("socket stream has no trait, close it ~n"),
+      gen_tcp:close(Socket);
+    {ok,{Ip,Port},Buffered_Packet} ->
+      case gen_tcp:connect(Ip,Port,?TCP_OPTIONS) of
+        {ok,To_Socket} ->
+          P1 = spawn(fun() -> socket_loop(Socket) end),
+          P2 = spawn(fun() -> socket_loop(To_Socket) end),
 
-      gen_tcp:controlling_process(Socket,P1),
-      gen_tcp:controlling_process(To_Socket,P2),
+          gen_tcp:controlling_process(Socket,P1),
+          gen_tcp:controlling_process(To_Socket,P2),
 
-      P1 ! {ready,P2},
-      P2 ! {ready,P1};
+          P1 ! {ready,P2},
+          P2 ! {ready_buffered,P1,Buffered_Packet};  % send buffered packet to destination when connected
 
-    {error,Reason} ->
-      io:format("connect dest ip with error ~p~n",[Reason]),
-      gen_tcp:close(Socket)
+        {error,Reason} ->
+          io:format("connect dest ip with error ~p~n",[Reason]),
+          gen_tcp:close(Socket)
+      end
   end.
 
 
+append_packet(Socket,Data) ->
+  case gen_tcp:recv(Socket,0) of
+        {ok,Packet} -> analyze_trait(Socket,<<Data/binary,Packet/binary>>);
+        {error,Reason} -> error
+  end.
+
+analyze_trait(Socket,Data) when byte_size(Data) < ?INIT_PACKET_THRESHOLD ->
+  {_,MP} = re:compile("(?i)service-provider:\\s*(\\w+)\\s*\r\n"),
+  case re:run(Data,MP,[{capture,[1],list}]) of
+    {match,[Key]} ->
+      case get_proxy_addr(Key) of
+        {ok,Addr} -> {ok,Addr,Data};
+        _ -> append_packet(Socket,Data)
+      end;
+    _ ->
+      append_packet(Socket,Data)
+  end;
+analyze_trait(Socket,_) ->
+  error.
+
+
+get_proxy_addr(Key) ->
+  Info = #{"xf" => {"127.0.0.1",9999}, "fx" => {"127.0.0.1",8000} },
+  maps:find(Key,Info).
 
 handle_cast(stop,State) ->
   {stop,normal,State}.
